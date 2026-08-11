7238f0120a5e77efe15156937007bee5ea8fa392
Rajeh Taher <rajeh@reforward.dev>
2026-08-11 21:40:35 +0300

    initial test module

---
+++ README.md
@@ -0,0 +1,7 @@
+# Go pseudo-version latest lab
+
+Disposable public module for testing whether `@latest` selects an untagged
+pseudo-version after every indexed semantic version is self-retracted and the
+Git tags are removed.
+
+This repository contains no production code.
---
+++ lab.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2026 Rajeh Taher
+//
+// Licensed under the MIT License. See LICENSE for details.
+
+// Package pseudolab exercises Go module proxy version selection.
+package pseudolab
+
+// Stage identifies the experiment stage represented by this commit.
+const Stage = "initial-release"
